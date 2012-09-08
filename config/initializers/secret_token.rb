f2015bd3965b9b4445f3b0551790f9d25f2240a3
Josh Buckley <buckleyjosh@gmail.com>
2012-09-08 12:46:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinoSite::Application.config.secret_token = '576c50765527ac57736a7f5e98635c7b143dcc6058aa7a4fd05c511a1aefa7f2df640f97394418eecab3f1f890779f1856d152e75cf6faeaf53be6cab672e6b5'
